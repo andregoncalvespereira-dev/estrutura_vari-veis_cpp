ca6eef45af635891e97cc6addb2b22c0abf27c4e
andregoncalvespereira-dev <andre.goncalves.pereira@escola.pr.gov.br>
2025-11-03 16:36:49 -0300

    tipos_variaveis

---
+++ tipos_variaveis.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    char letra = 'A';
+    bool ativo= true;
+    float preco=3.50;
+    string nome= "andre";
+    int quantidade= 16;
+    cout<< "A quan tidade do preço é: " << preco <<endl;
+    cout<< "A quantidade é: " << quantidade<< endl;
+    cout<< "A alternativa é: "<<letra <<endl;
+    cout<< "O verdadeiro é " << ativo<<endl;
+    cout<< "O meu nome é: "<< nome;
+
+    return 0;
+}
